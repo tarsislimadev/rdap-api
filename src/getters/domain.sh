c09725f39efa07efbdc465780fab6b7f073ea728
Tarsis <br.tmvdl@gmail.com>
2023-09-08 16:35:58 -0300

    Updating getter files

--- src/getters/domain.sh
+++ src/getters/domain.sh
@@ -4,30 +4,16 @@
 
 . .env
 
-domain_id=$( date +%Y%m%d%H%M%S )
-
-echo
-echo "domain_id: ${domain_id}"
-echo
+type="domain"
 
 domain="${1}"
 
-echo
-echo "domain: ${domain}"
-echo 
-
 # runner
 
-type="domain"
-
-object="google.com.br"
-
-resp=$( curl -sL "https://rdap.org/${type}/${object}" | jq )
+resp=$( curl -sL "https://rdap.org/${type}/${domain}" | jq )
 
 # outputs
 
 bash create.sh "domains" "${domain}" "domain" "${domain}"
 
 bash create.sh "domains" "${domain}" "resp" "${resp}"
-
-# echo "${resp}"
